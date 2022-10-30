8da1902d81701faa3619dade5d979836cd3c7baf
burtsevpav <pasha.burtsev.2015@mail.ru>
2022-10-30 14:21:28 +0300

    Добавили pop-ups с формами

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,20 +4,21 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 // Вывод ошибок при заполнении формы
-const formItem = document.forms.form;
-const formEmail = formItem.email;
-const formName = formItem.name;
-const formError = document.querySelector(`.form-column__error`);
-formItem.addEventListener("submit", formCancelFn);
-function formCancelFn(e) {
-  if (!formEmail.value & !formName.value) {
-    e.preventDefault();
-    formError.textContent = "Fill the form!";
-  } else if (!formEmail.value) {
-    e.preventDefault();
-    formError.textContent = "Enter your e-mail!";
-  } else if (!formName.value) {
-    e.preventDefault();
-    formError.textContent = "Enter your name!";
-  }
-}
+// const formItem = document.forms.form;
+// const formEmail = formItem.email;
+// const formName = formItem.name;
+// const formError = document.querySelector(`.form-column__error`);
+
+// formItem.addEventListener("submit", formCancelFn);
+// function formCancelFn(e) {
+//   if (!formEmail.value & !formName.value) {
+//     e.preventDefault();
+//     formError.textContent = "Fill the form!";
+//   } else if (!formEmail.value) {
+//     e.preventDefault();
+//     formError.textContent = "Enter your e-mail!";
+//   } else if (!formName.value) {
+//     e.preventDefault();
+//     formError.textContent = "Enter your name!";
+//   }
+// }
